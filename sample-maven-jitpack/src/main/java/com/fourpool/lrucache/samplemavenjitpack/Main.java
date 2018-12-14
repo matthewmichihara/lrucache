7d2d0847069f7e8a24418c5e9495c67d9d0a9efd
Matthew Michihara <matthew.michihara@gmail.com>
2018-12-13 22:00:30 -0500

    Simplify some samples

--- sample-maven-jitpack/src/main/java/com/fourpool/lrucache/samplemavenjitpack/Main.java
+++ sample-maven-jitpack/src/main/java/com/fourpool/lrucache/samplemavenjitpack/Main.java
@@ -3,21 +3,18 @@ package com.fourpool.lrucache.samplemavenjitpack;
 import com.fourpool.lrucache.LruCache;
 
 public class Main {
-  static class Summer {
-    @LruCache
-    int sum(int a, int b) {
-      System.out.println("sum called");
-      return a + b;
-    }
+  @LruCache
+  static int sum(int a, int b) {
+    System.out.println("sum called");
+    return a + b;
   }
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    Summer summer = new Summer();
-    System.out.println(summer.sum(1, 2));
-    System.out.println(summer.sum(1, 2));
-    System.out.println(summer.sum(1, 2));
-    System.out.println(summer.sum(1, 2));
-    System.out.println(summer.sum(1, 2));
+    System.out.println(sum(1, 2));
+    System.out.println(sum(1, 2));
+    System.out.println(sum(1, 2));
+    System.out.println(sum(1, 2));
+    System.out.println(sum(1, 2));
   }
 }
